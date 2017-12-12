50fc6ef6b924c045697a3709a4ae37618c6e9857
Nicolas Hovart <kola.hov@gmail.com>
2017-12-12 01:10:18 +0100

    WIP ACtionMailer ...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  # before_action :authenticate_user!
+  
   def index
+    @user = current_user
   end
+  
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
     default from: 'notifications@mabite.com'
     
-     def welcome_email(user)
-       @user = user
+     def welcome_email(email)
+       
        @url  = 'http://example.com/login'
-       mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+       mail(to: email, subject: 'Welcome to My Awesome Site')
      end
 end
