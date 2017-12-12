c6b1e6c3607fcb3355abe7741144ee1bb8009aad
Nicolas Hovart <kola.hov@gmail.com>
2017-12-12 15:21:07 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+@u = User.create(email:"kola@yopmail.com", name:"Jokoss le boloss")
+@u1 = User.create(email:"koli@yopmail.com", name:"Jakass la balass")
+@u2 = User.create(email:"kolu@yopmail.com", name:"Jakiss la baliss")
+@u3 = User.create(email:"kolo@yopmail.com", name:"Jakoss la baloss")
+@u4 = User.create(email:"kolux@yopmail.com", name:"Jakux la balux")
+
+@users = []
+User.all.each do |user|
+	@users << user
+end
+
